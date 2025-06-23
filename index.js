982fbe9b4b61f790a95172f862c50edf18ba392f
Martin Muthaura <mmuthaura 06@gmail.com>
2025-06-23 19:09:56 +0300

    Complete variables lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+ // companyName should be a const
+const companyName = "Scuber";
+
+// let for mostProfitableNeighborhood (since value can change)
+let mostProfitableNeighborhood = "Chelsea";
+
+// let for companyCeo (since value can change)
+let companyCeo = "Susan Smith";
